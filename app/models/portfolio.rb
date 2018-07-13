01ade3b2d4fef38f52b2832bc49a2e1f382c1bf4
Drew Nguyen <andrew.tony.nguyen@gmail.com>
2018-07-13 14:54:14 -0700

    Implemented form configuration for nested attributes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,9 @@
 class Portfolio < ApplicationRecord
   include Placeholder
+  has_many :technologies
+  accepts_nested_attributes_for :technologies, 
+                                reject_if: lambda { |attrs| attrs['name'].blank? }
+
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
@@ -15,5 +19,4 @@ class Portfolio < ApplicationRecord
     self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200');
   end
 
-  has_many :technologies
 end
